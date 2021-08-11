1ed17d05484e07b9942b6475143ad3e4cdc4b9a4
WasemIdres <thewasem7915@gmail.com>
2021-08-11 14:24:02 +0300

    first update

--- js/cart.js
+++ js/cart.js
@@ -19,24 +19,39 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() { }
+function clearCart() {}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
+let arr = [];
+
 function showCart() {
 
     // TODO: Find the table body
+    // let bodyElement = document.createElement('tbody');
 
     // TODO: Iterate over the items in the cart
+    // for (let i = 0; i < Product.allProducts.length; i++) {
+    //     arr = Product.allProducts[i];
+    // }
     // TODO: Create a TR
-    // TODO: Create a TD for the delete link, quantity,  and the item
-    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    // let rowElement = document.createElement('tr');
 
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    // let tableElement = document.createElement('td');
+    // for (let i = 0; i < Product.allProducts.length; i++) {
+    //     tableElement.textContent = Product.allProducts[i].name;
+    // }
+    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    // bodyElement.appendChild(rowElement);
+    // rowElement.appendChild(tableElement);
 }
 
 function removeItemFromCart(event) {
 
     // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+
     // TODO: Save the cart back to local storage
+    cart.event.saveToLocalStorage();
     // TODO: Re-draw the cart table
 
 }
